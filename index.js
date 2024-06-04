2917a9ea19d19c29bb1b57762dc1ef0afbe56fb1
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-06-04 21:07:59 +0600

    added get boidatas id

--- index.js
+++ index.js
@@ -1,73 +1,102 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // middleware
 
- app.use(cors());
+app.use(cors());
 
-
- const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.edk1eij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.edk1eij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      const premiumMemberCollection = client.db("Assignment-no-12").collection("premium-member");
-      const successStoryCollection = client.db("Assignment-no-12").collection("successStory");
-      app.get('/premiumMember' , async(req,res)=>{
-        const {order} = req.query;
-        let sort = {};
-        if(order === "descending"){
-           sort = {age: -1};
-        }
-        if(order === "ascending"){
-           sort = {age: 1};
-        }
-        const result = await premiumMemberCollection.find().sort(sort).toArray();
-        res.send(result);
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    const premiumMemberCollection = client
+      .db("Assignment-no-12")
+      .collection("premium-member");
+    const successStoryCollection = client
+      .db("Assignment-no-12")
+      .collection("successStory");
+      const boiDatasCollection = client
+      .db("Assignment-no-12")
+      .collection("boiDatas");
+    app.get("/premiumMember", async (req, res) => {
+      const { order } = req.query;
+      let sort = {};
+      if (order === "descending") {
+        sort = { age: -1 };
+      }
+      if (order === "ascending") {
+        sort = { age: 1 };
+      }
+      const result = await premiumMemberCollection.find().sort(sort).toArray();
+      res.send(result);
     });
 
-    app.get('/successStory' , async (req , res) => {
-      const {order} = req.query;
-        let sort = {};
-        if(order === "descending"){
-           sort = {marriageDate: -1};
-        }
-        if(order === "ascending"){
-           sort = {marriageDate: 1};
-        }
-      const result = await successStoryCollection.find().sort(sort).toArray();
-      res.send(result)
-    })
+    app.get("/successStory", async (req, res) => {
+      const { order } = req.query;
+      let sort = {};
+      if (order === "descending") {
+        sort = { marriageDate: -1 };
+      }
+      if (order === "ascending") {
+        sort = { marriageDate: 1 };
+      }
+      const result = await successStoryCollection.find().sort().toArray();
+      res.send(result);
+    });
 
+    app.get("/boiDatas", async (req, res) => {
+      const { ageMax, ageMin, division, type } = req.query;
+      let query = {};
 
+      // Check for age range
+      if (ageMin && ageMax) {
+        query.age = { $gte: parseInt(ageMin), $lte: parseInt(ageMax) };
+      } else if (ageMin) {
+        query.age = { $gte: parseInt(ageMin) };
+      } else if (ageMax) {
+        query.age = { $lte: parseInt(ageMax) };
+      }
 
-    
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-   
-    }
+      // Check for biodata_type
+      if (type) {
+        query.biodata_type = type;
+      }
+
+      // Check for permanent_division_name
+      if (division) {
+        query.permanent_division_name = division;
+      }
+
+      const result = await boiDatasCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-
- app.get('/', (req, res) => {
-    res.send('Matrimony server side Home page running');
- });
- app.listen(port , ()=>{
-    console.log(`Example app listening on port ${port}`)
- });
+app.get("/", (req, res) => {
+  res.send("Matrimony server side Home page running");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
